4506c06e5061c5ad234abf13bfc66018093b3e99
Ranjeet Jaryal Bhatt <ranjeetjaryalbhattbhatt@gmail.com>
2025-05-01 02:47:55 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Medical chatbot",
+    version = "0.0.0",
+    author= "ranjeetjb",
+    author_email= "ranjeetjaryalbhattbhatt@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
