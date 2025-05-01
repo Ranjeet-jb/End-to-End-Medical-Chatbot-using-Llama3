2429031e1b83c828060fd28c036df8c4ddda27b4
Ranjeet Jaryal Bhatt <ranjeetjaryalbhattbhatt@gmail.com>
2025-05-02 03:15:44 +0530

    prompt added

--- src/prompt.py
+++ src/prompt.py
@@ -1,7 +1,6 @@
 system_prompt = (
     "Answer the question using only the given context. "
-    "Only explain one or two lines"
-    "and"
+    "Do not explain or add reasoning. "
     "Return only the final answer, in one or two sentences. "
     "If the answer is not in the context, respond with 'I don't know.'\n\n"
     "Context:\n{context}"
