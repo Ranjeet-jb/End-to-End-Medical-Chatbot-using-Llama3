792b97778af7eb15e8592d9d675a15a5d0e8e37a
Ranjeet Jaryal Bhatt <ranjeetjaryalbhattbhatt@gmail.com>
2025-05-02 01:45:12 +0530

    prompt added

--- src/prompt.py
+++ src/prompt.py
@@ -1,18 +1,8 @@
-from store_index import docsearch
-
-retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":5})
-
-retrieved_docs = retriever.invoke("HIV")
-
-# Join the retrieved documents into a single string
-retrieved_text = "\n".join([doc.page_content for doc in retrieved_docs])
-
-# Prepare the system prompt for the model
 system_prompt = (
-    "You are an assistant for question-answering tasks. "
-    "Use the following pieces of retrieved context to answer the question. "
-    "If you don't know the answer, say that you don't know. "
-    "Use three sentences maximum and keep the answer concise."
-    "\n\n"
-    f"{retrieved_text}"
+    "Answer the question using only the given context. "
+    "Only explain one or two lines"
+    "and"
+    "Return only the final answer, in one or two sentences. "
+    "If the answer is not in the context, respond with 'I don't know.'\n\n"
+    "Context:\n{context}"
 )
