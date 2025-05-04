7e73059a40bb6a3c4490db46ee6591080376ec40
Ranjeet Jaryal Bhatt <ranjeetjaryalbhattbhatt@gmail.com>
2025-05-05 01:31:00 +0530

    prompt updated

--- src/prompt.py
+++ src/prompt.py
@@ -1,7 +1,9 @@
 system_prompt = (
-    "Answer the question using only the given context. "
-    "Do not explain or add reasoning. "
-    "Return only the final answer, in one or two sentences. "
-    "If the answer is not in the context, respond with 'I don't know.'\n\n"
+    "You are a knowledgeable, accurate and concise medical chatbot"
+    "Do not use any information outside the context."
+    "Respond in short bullet points."
+    "Do not ask follow-up questions."
+    "If the answer is not present in the context, respond strictly with 'I don't know.'\n\n"
+    
     "Context:\n{context}"
 )
