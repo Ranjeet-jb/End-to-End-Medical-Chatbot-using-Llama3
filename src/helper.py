9300c63a14309c8bcfb7c8d25b5856b6acb50c84
Ranjeet Jaryal Bhatt <ranjeetjaryalbhattbhatt@gmail.com>
2025-05-07 20:27:45 +0530

    code updatec

--- src/helper.py
+++ src/helper.py
@@ -1,6 +1,7 @@
-from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 
 def loadpdf(data):
@@ -10,16 +11,12 @@ def loadpdf(data):
     documents = loader.load()
     return documents
 
-
 # create text chunks 
 def text_split(extracted_data):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 20)
     text_chunks = text_splitter.split_documents(extracted_data)
 
     return text_chunks
-
-
-from langchain_huggingface import HuggingFaceEmbeddings
 
 def download_huggingface_embeddings():
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
